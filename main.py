a73744a5196597818d14026662b97103044a1e77
Erkan Celen <erkancelen@MacBook-Pro.local>
2022-09-05 22:12:33 +0200

    improvements

--- main.py
+++ main.py
@@ -21,7 +21,8 @@ def test():
 def game_report():
     summoner_name = request.form['summoner_name']
     api = riot_api()
-    matches = api.get_matches_by_summoner_name(summoner_name)
+    summoner = api.get_summoner_by_name(summoner_name)
+    matches = api.get_matches_by_summoner_name(summoner['name'])
     last_game = matches[0]
     game = last_game
     champions, chart_data_js, labels = data_formation.game_report_data(game)
