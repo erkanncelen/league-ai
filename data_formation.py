4044215a00a8455b907e8def82e813fdebbe7d83
Erkan Celen <erkancelen@MacBook-Pro-2.local>
2022-09-11 21:25:20 +0200

    added bar and line charts and toggling functionality

--- data_formation.py
+++ data_formation.py
@@ -3,15 +3,17 @@ import numpy as np
 from riot_api_connection import riot_api
 import json
 
-def game_to_df(game):
-    columns = pd.DataFrame.from_dict(game['info']['participants'][0]).set_index('participantId').columns
+def game_to_df(game) -> pd.DataFrame:
+
+    columns = pd.DataFrame.from_dict(game['info']['participants'][:1]).columns
     df = pd.DataFrame(columns=columns)
     for item in game['info']['participants']:
-        temp = pd.DataFrame.from_dict(item).set_index('participantId')[:1]
+        temp = pd.DataFrame(item).reset_index(drop=True)[:1]
         df = pd.concat([df, temp])
     return df
 
-def game_timeline_to_df(game_timeline):
+def game_timeline_to_df(game_timeline, game_df):
+
     df = pd.DataFrame.from_dict(game_timeline)
     frames_info = pd.DataFrame.from_dict(df.loc['frames']['info'])
     # Expand timeline data into a final dataframe
@@ -21,6 +23,8 @@ def game_timeline_to_df(game_timeline):
         df['timeframe'] = i
         frames = [game_timeline_df, df]
         game_timeline_df = pd.concat(frames)
+    game_timeline_df = game_timeline_df.merge(game_df[['participantId', 'championName', 'win', 'teamId', 'teamPosition']], on='participantId', how='left')
+    game_timeline_df = game_timeline_df.sort_values(by=['timeframe', 'teamId','teamPosition'])
     # Expand championStats column
     keys = game_timeline_df['championStats'][0].keys()
     for key in keys:
@@ -35,28 +39,8 @@ def game_timeline_to_df(game_timeline):
 
     return game_timeline_df
 
-def game_report_data(game):
-    api = riot_api()
-    game = api.get_match_info_by_match_id(game)
-    game_df = game_to_df(game)[[
-        'championName',
-        'championId',
-        'teamId',
-        'teamPosition',
-        'kills',
-        'assists',
-        'deaths',
-        'totalTimeSpentDead',
-        'longestTimeSpentLiving',
-        'goldEarned',
-        'timeCCingOthers',
-        'visionScore',
-        'totalDamageDealtToChampions',
-        'damageDealtToObjectives',
-        'damageDealtToBuildings',
-        'totalHealsOnTeammates',
-        'totalDamageShieldedOnTeammates',
-    ]]
+def game_report_data(game_df):
+
     game_df = game_df.sort_values(by=['teamId','teamPosition'])
     champions = game_df['championName'].to_list()
     metrics = [
@@ -77,12 +61,50 @@ def game_report_data(game):
             game_df[f'{metric}_normalized'] = (game_df[metric]/(game_df[metric].sum()))*100
         metrics_normalized.append(f'{metric}_normalized')
     chart_data = {}
-    for i in range(len(game_df['championName'])):
-        chart_data[i] = game_df.iloc[i][metrics_normalized]
+    for champion in list(game_df['championName']):
+        chart_data[champion] = game_df[game_df['championName'] == champion][metrics_normalized].iloc[0]
     labels = ['Damage', 'Objectives', 'Buildings', 'Vision', 'CC', 'Heal & Shield', 'Gold']
 
     chart_data_js = []
-    for i in range(len(chart_data)):
-        chart_data_js.append(np.array2string(chart_data[i].values, separator=', '))
+    for key in chart_data.keys():
+        chart_data_js.append(np.array2string(chart_data[key].values, separator=', '))
 
-    return champions, chart_data_js, labels
+    metrics_to_medals = {
+        'totalDamageDealtToChampions': "Terminator",
+        'damageDealtToObjectives': "Dragon Slayer",
+        'damageDealtToBuildings': "Bulldozer",
+        'visionScore': "Visionaire",
+        'timeCCingOthers': "Mr. Policeman",
+        'HealsAndShieldOnTeammates': "Nurse",
+        'goldEarned': "Rockefeller",
+    }
+
+    champion_medals = {}
+    for champion in chart_data.keys():
+        medals = []
+        helper = 0
+        for metric in metrics_to_medals.keys():
+            if chart_data[champion][f"{metric}_normalized"] >= 15:
+                medals.append(metrics_to_medals[metric])
+            if chart_data[champion][f"{metric}_normalized"] >= 10:
+                helper+= 1
+        if len(medals) > 0:        
+            champion_medals[champion] = medals
+        elif helper >= 2:
+            champion_medals[champion] = ['Average Dude']
+        else:
+            champion_medals[champion] = ['Impostor']
+
+    medal_definitions = {
+        "Terminator": "<b>Terminator</b><hr>This champion dealt incredible damage to opponents in this game.",
+        "Nurse": "<b>Nurse</b><hr>This champion healed and shielded teammates a lot.",
+        "Bulldozer": "<b>Bulldozer</b><hr>This champion dealt a lot of damage to buildings. Remember you must take down buildings to win the game.",
+        "Dragon Slayer": "<b>Dragon Slayer</b><hr>This champion beat the shit out of epic neutral monsters to secure objectives.",
+        "Visionaire": "<b>Visionaire</b><hr>This champion had an incredible vision score which gave the team an information advantage.",
+        "Rockefeller": "<b>Rockefeller</b><hr>This champion earned tons of gold in this game. Were they well-spent? That's another question.",
+        "Mr. Policeman": "<b>Mr. Policeman</b><hr>This player did a lot of crowd control and handcuffed opponents during the game. They were annoyed for sure.",
+        "Average Dude": "<b>Average Dude</b><hr>Although not too bad, this player did not have any major impact in this game.",
+        "Impostor": "<b>Impostor</b><hr>Only God knows what this player did during the game.",
+    }
+
+    return champions, champion_medals, medal_definitions, chart_data_js, labels
--- main.py
+++ main.py
@@ -3,8 +3,12 @@ from flask import request, escape
 import pandas as pd
 import numpy as np
 import logging
-from riot_api_connection import riot_api
 import json
+
+import plotly.express as px
+import plotly.io as pio
+
+from riot_api_connection import riot_api
 import data_formation
 
 app = Flask(__name__)
@@ -24,26 +28,74 @@ def game_report():
     summoner = api.get_summoner_by_name(summoner_name)
     matches = api.get_matches_by_summoner_name(summoner['name'])
     last_game = matches[0]
-    game = last_game
-    champions, chart_data_js, labels = data_formation.game_report_data(game)
-    game_timeline = api.get_game_timeline_by_match_id(game)
-    game_timeline_df = data_formation.game_timeline_to_df(game_timeline)
-    summoner1_positions = json.dumps(list(game_timeline_df[game_timeline_df['participantId'].isin([1])]['position'])).replace('"','')
-    summoner2_positions = json.dumps(list(game_timeline_df[game_timeline_df['participantId'].isin([2])]['position'])).replace('"','')
-    summoner3_positions = json.dumps(list(game_timeline_df[game_timeline_df['participantId'].isin([3])]['position'])).replace('"','')
-    summoner4_positions = json.dumps(list(game_timeline_df[game_timeline_df['participantId'].isin([4])]['position'])).replace('"','')
-    summoner5_positions = json.dumps(list(game_timeline_df[game_timeline_df['participantId'].isin([5])]['position'])).replace('"','')
-    summoner6_positions = json.dumps(list(game_timeline_df[game_timeline_df['participantId'].isin([6])]['position'])).replace('"','')
-    summoner7_positions = json.dumps(list(game_timeline_df[game_timeline_df['participantId'].isin([7])]['position'])).replace('"','')
-    summoner8_positions = json.dumps(list(game_timeline_df[game_timeline_df['participantId'].isin([8])]['position'])).replace('"','')
-    summoner9_positions = json.dumps(list(game_timeline_df[game_timeline_df['participantId'].isin([9])]['position'])).replace('"','')
-    summoner10_positions = json.dumps(list(game_timeline_df[game_timeline_df['participantId'].isin([10])]['position'])).replace('"','')
+    game = api.get_match_info_by_match_id(last_game)
+    game_df = data_formation.game_to_df(game)
 
-
+    champions, champion_medals, medal_definitions, chart_data_js, labels = data_formation.game_report_data(game_df)
     
+    game_timeline = api.get_game_timeline_by_match_id(last_game)
+
+    game_timeline_df = data_formation.game_timeline_to_df(game_timeline, game_df)
+    summoner1_positions = json.dumps(list(game_timeline_df[game_timeline_df['championName'].isin([champions[0]])]['position'])).replace('"','')
+    summoner2_positions = json.dumps(list(game_timeline_df[game_timeline_df['championName'].isin([champions[1]])]['position'])).replace('"','')
+    summoner3_positions = json.dumps(list(game_timeline_df[game_timeline_df['championName'].isin([champions[2]])]['position'])).replace('"','')
+    summoner4_positions = json.dumps(list(game_timeline_df[game_timeline_df['championName'].isin([champions[3]])]['position'])).replace('"','')
+    summoner5_positions = json.dumps(list(game_timeline_df[game_timeline_df['championName'].isin([champions[4]])]['position'])).replace('"','')
+    summoner6_positions = json.dumps(list(game_timeline_df[game_timeline_df['championName'].isin([champions[5]])]['position'])).replace('"','')
+    summoner7_positions = json.dumps(list(game_timeline_df[game_timeline_df['championName'].isin([champions[6]])]['position'])).replace('"','')
+    summoner8_positions = json.dumps(list(game_timeline_df[game_timeline_df['championName'].isin([champions[7]])]['position'])).replace('"','')
+    summoner9_positions = json.dumps(list(game_timeline_df[game_timeline_df['championName'].isin([champions[8]])]['position'])).replace('"','')
+    summoner10_positions = json.dumps(list(game_timeline_df[game_timeline_df['championName'].isin([champions[9]])]['position'])).replace('"','')
+
+    pio.templates.default = "plotly_dark"
+    layout = {'plot_bgcolor': "rgba(0, 0, 0, 0)",'paper_bgcolor': "rgba(0, 0, 0, 0.5)",'legend_bgcolor': "rgba(0, 0, 0, 0)"}
+
+    line_charts = {
+        "totalDamageDoneToChampions": "Total Damage to Champions",
+        "totalGold": "Gold Earned",
+        "xp": "Experience Points",
+        "level": "Champion Level",
+    }
+    plotly_line_charts = {}
+
+    for key in line_charts.keys():
+        fig = px.line(game_timeline_df, x="timeframe", y=f"{key}", color='championName', title=line_charts[key])
+        fig.update_layout(layout)
+        fig.update_layout(legend=dict(yanchor="top", y=0.99, xanchor="left", x=0.01), legend_title_text='', title_x=0.5)
+        fig.update_xaxes(title_text='')
+        fig.update_yaxes(title_text='')
+        chart_html = pio.to_html(fig,full_html=False, include_plotlyjs=False, config = {'displayModeBar': False})
+        plotly_line_charts[key] = chart_html
+
+
+    bar_charts = {
+        "totalDamageDealtToChampions": "Total Damage to Champions",
+        "damageDealtToObjectives": "Total Damage to Objectives",
+        "damageDealtToBuildings": "Total Damage to Buildings",
+        "damageSelfMitigated": "Self Mitigated Damage",
+        "totalMinionsKilled": "Total Minions Killed",
+        "goldEarned": "Gold Earned",
+        "visionScore": "Vision Score",
+        "timeCCingOthers": "Time CCing Others",
+        "totalTimeSpentDead": "Total Time Spent Dead",
+        "longestTimeSpentLiving": "Longest Time Spent Living",
+    }
+
+    plotly_bar_charts = {}
+
+    for key in bar_charts.keys():
+        fig = px.bar(game_df, y="championName", x=f"{key}", color='teamId', orientation='h', title=bar_charts[key])
+        fig.update_layout(layout)
+        fig.update_layout(showlegend=False, title_x=0.5)
+        fig.update_xaxes(title_text='')
+        fig.update_yaxes(title_text='')
+        chart_html = pio.to_html(fig,full_html=False, include_plotlyjs=False, config = {'displayModeBar': False})
+        plotly_bar_charts[key] = chart_html
     
     return render_template('game_report.html',
     champions = champions,
+    champion_medals = champion_medals, 
+    medal_definitions = medal_definitions,
     chart_data_js = chart_data_js,
     labels = labels,
 
@@ -56,7 +108,13 @@ def game_report():
     summoner7_positions = summoner7_positions,
     summoner8_positions = summoner8_positions,
     summoner9_positions = summoner9_positions,
-    summoner10_positions = summoner10_positions
+    summoner10_positions = summoner10_positions,
+
+    plotly_line_charts = plotly_line_charts,
+    line_chart_keys = line_charts.keys(),
+    plotly_bar_charts = plotly_bar_charts,
+    bar_chart_keys = bar_charts.keys()
+    
     )
 
 
